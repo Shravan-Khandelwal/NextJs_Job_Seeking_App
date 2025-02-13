66a45a15aa5f52265c0702607b9a35175619c023
Shravan_Khandelwal <102209703+Shravan-Khandelwal@users.noreply.github.com>
2025-02-13 21:55:46 +0530

    Update AppContext.jsx

--- my-next-app/Store/AppContext.jsx
+++ my-next-app/Store/AppContext.jsx
@@ -49,7 +49,7 @@ function AppContextProvider({ children }) {
     //   const cookieArr = document.cookie.split(";");
     //   console.log("COOKIE ARRAY");
     //   console.log(cookieArr);
-      
+
     //   for (let cookie of cookieArr) {
     //     const [key, value] = cookie.trim().split("=");
     //     if (key === name) {
@@ -59,7 +59,7 @@ function AppContextProvider({ children }) {
     //   return null;
     // };
 
-     const fetchToken = async () => {
+    const fetchToken = async () => {
       try {
         const res = await fetch(
           "https://nextjs-job-seeking-app.onrender.com/api/auth/check-login",
@@ -86,247 +86,232 @@ function AppContextProvider({ children }) {
         setUserDetails(null);
       }
 
-       
+      // const jwtToken = getCookie("token");
+      //  console.log("JWT Token");
+      //   console.log(jwtToken);
 
-    // const jwtToken = getCookie("token");
-    //  console.log("JWT Token");
-    //   console.log(jwtToken);
+      // if (jwtToken) {
+      //   const decoded = jwtDecode(jwtToken);
 
-    // if (jwtToken) {
-    //   const decoded = jwtDecode(jwtToken);
+      //   setUserDetails(decoded.userData); // Assuming the token contains a `user` field
+      // } else {
+      //   setUserDetails(null);
+      //   console.log("No token found");
+      // }
 
-    //   setUserDetails(decoded.userData); // Assuming the token contains a `user` field
-    // } else {
-    //   setUserDetails(null);
-    //   console.log("No token found");
-    // }
-
-
-
-fetchToken();
-       
+      fetchToken();
+    };
   }, [IsLoggedIn]);
+}
 
-
-
-
-
-
-
-
-
-    
-
-  // ! Fetching The HR Jobs Listing
-  useEffect(() => {
-    async function Fetch_HR_Jobs_Listing() {
-      try {
-        const response = await fetch(
-          "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/getMyJobs",
-          { method: "GET", credentials: "include" }
-        );
-
-        const result = await response.json();
-
-        if (response.ok) {
-          // console.log("HR JOBS ARE  IS ");
-          setHR_JobsListings(result.MyJobsList);
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
-    Fetch_HR_Jobs_Listing();
-  }, [NewJobPostedTracker, UserDetails, DeleteJobTracker]);
-
-  //! FUNCTION TO FETCH ALL THE EXISTING JOBS
-  useEffect(() => {
-    async function fetchAllJobs() {
-      try {
-        const response = await fetch(
-          "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/getAllJobs",
-          {
-            method: "GET",
-            credentials: "include",
-          }
-        );
-
-        const result = await response.json();
-        // console.log("RESULT IS ");
-        if (response.ok) {
-          // console.log("RESULT IS ");
-
-          // console.log(result);
-          setAllJobs(result.jobList);
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
-    fetchAllJobs();
-  }, [NewJobPostedTracker, IsLoggedIn, DeleteJobTracker]);
-
-  //! Function For Searching A Job By Its Name
-  function SearchJobs(SearchJobText) {
-    if (SearchJobText) {
-      const SearchedJob = AllJobs?.filter(
-        (job) => job.JobTitle.toLowerCase() == SearchJobText.toLowerCase()
+// ! Fetching The HR Jobs Listing
+useEffect(() => {
+  async function Fetch_HR_Jobs_Listing() {
+    try {
+      const response = await fetch(
+        "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/getMyJobs",
+        { method: "GET", credentials: "include" }
       );
-      if (SearchedJob.length != 0) {
-        setSearchJobValues(SearchedJob || []);
+
+      const result = await response.json();
+
+      if (response.ok) {
+        // console.log("HR JOBS ARE  IS ");
+        setHR_JobsListings(result.MyJobsList);
       }
+    } catch (error) {
+      console.log(error.message);
     }
   }
+  Fetch_HR_Jobs_Listing();
+}, [NewJobPostedTracker, UserDetails, DeleteJobTracker]);
 
-  //! FUNCTION FOR FILTERING THE JOBS BY CATEGORY
-  useEffect(() => {
-    function FilterJobsByCategory() {
-      // console.log("VAL IS " + JobCategory);
-      if (JobCategory) {
-        const FilteredJobsByCategory = AllJobs?.filter(
-          (Job) => Job.JobCategory.toLowerCase() === JobCategory.toLowerCase()
-        );
-
-        // console.log("FILTERED");
-        // console.log(FilteredJobsByCategory);
-
-        setFilteredJobCategory(FilteredJobsByCategory);
-      }
-    }
-    FilterJobsByCategory();
-  }, [JobCategory]);
-
-  //! FUNCTION FOR FILTERING THE JOBS BY SALARY RANGE
-  useEffect(() => {
-    function FilterBySalary() {
-
-      // fixedSalary
-      const FilteredValues = AllJobs?.filter(
-        (Job) =>
-          Job.fixedSalary >= FilteredSalaryValue.StartRange &&
-          Job.fixedSalary <= FilteredSalaryValue.EndRange
-      );
-
-      setFilteredBySalaryJobs(FilteredValues);
-    }
-    FilterBySalary();
-  }, [FilteredSalaryValue]);
-
-  //! FUNCTION FOR FETCHING THE LIST OF ALL THE COMPANIES THE USER HAS APPLIED TO
-  useEffect(() => {
-    async function FetchCompaniesList() {
-      try {
-        const response = await fetch(
-          "https://nextjs-job-seeking-app.onrender.com/api/userRoutes/FetchCompaniesList",
-          {
-            method: "GET",
-            credentials: "include",
-          }
-        );
-
-        const result = await response.json();
-
-        // if (result.JobList.length > 0) {
-        //   result.JobList.map((Job) => {
-        //     const JobDetails = await fetch(
-        //       "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/FindJobByID/:id"
-        //     );
-        //   });
-        // }
-
-        if (response.ok) {
-          setJobsAppliedTo(result.JobList);
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
-    FetchCompaniesList();
-  }, []);
-
-  //! FUNCTION FOR POSTING AN APPLICATION
-  async function SubmitJobApplication(
-    Name,
-    Email,
-    Resume,
-    Phone,
-    Address,
-    coverLetter,
-    jobId
-  ) {
+//! FUNCTION TO FETCH ALL THE EXISTING JOBS
+useEffect(() => {
+  async function fetchAllJobs() {
     try {
-      // Prepare FormData to handle file uploads
-      const formData = new FormData();
-      formData.append("Name", Name);
-      formData.append("Email", Email);
-      formData.append("Phone", Phone);
-      formData.append("Address", Address);
-      formData.append("coverLetter", coverLetter);
-      formData.append("Resume", Resume); // Add the file
-      formData.append("jobId", jobId);
-
       const response = await fetch(
-        "https://nextjs-job-seeking-app.onrender.com/api/applicationRoutes/postApplication",
+        "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/getAllJobs",
         {
-          method: "POST",
+          method: "GET",
           credentials: "include",
-          body: formData,
         }
       );
 
       const result = await response.json();
+      // console.log("RESULT IS ");
       if (response.ok) {
-        setNewJobApplication(!NewJobApplication);
-        router.push("/");
-        toast.success("Successfully Applied!!");
-      } else {
-        toast.error(result.message);
+        // console.log("RESULT IS ");
+
+        // console.log(result);
+        setAllJobs(result.jobList);
       }
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
   }
+  fetchAllJobs();
+}, [NewJobPostedTracker, IsLoggedIn, DeleteJobTracker]);
 
-  return (
-    <AppContext.Provider
-      value={{
-        JobsAppliedTo,
-        setUserDetails,
-        UserDetails,
-        setIsLoggedIn,
-        setNewJobPostedTracker,
-        HR_JobsListings,
-        AllJobs,
-        setDeleteJobTracker,
-        DeleteJobTracker,
-        setSearchJobText,
-        SearchJobValues,
-        setSearchJobValues,
-        SearchJobText,
-        SearchJobs,
-        setJobCategory,
-        FilteredJobCategory,
-        setIsFilteredByCategory,
-        IsFilteredByCategory,
-        setIsSearch,
-        IsSearch,
-        setIsLoading,
-        isLoading,
-        setFilteredSalaryValue,
-        FilteredSalaryValue,
-        FilteredBySalaryJobs,
-        setIsFilterBySalary,
-        IsFilterBySalary,
-        SubmitJobApplication,
-        setNewJobApplication,
-        NewJobApplication,
-        setListOfJobApplications,
-        ListOfJobApplications,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+//! Function For Searching A Job By Its Name
+function SearchJobs(SearchJobText) {
+  if (SearchJobText) {
+    const SearchedJob = AllJobs?.filter(
+      (job) => job.JobTitle.toLowerCase() == SearchJobText.toLowerCase()
+    );
+    if (SearchedJob.length != 0) {
+      setSearchJobValues(SearchedJob || []);
+    }
+  }
 }
+
+//! FUNCTION FOR FILTERING THE JOBS BY CATEGORY
+useEffect(() => {
+  function FilterJobsByCategory() {
+    // console.log("VAL IS " + JobCategory);
+    if (JobCategory) {
+      const FilteredJobsByCategory = AllJobs?.filter(
+        (Job) => Job.JobCategory.toLowerCase() === JobCategory.toLowerCase()
+      );
+
+      // console.log("FILTERED");
+      // console.log(FilteredJobsByCategory);
+
+      setFilteredJobCategory(FilteredJobsByCategory);
+    }
+  }
+  FilterJobsByCategory();
+}, [JobCategory]);
+
+//! FUNCTION FOR FILTERING THE JOBS BY SALARY RANGE
+useEffect(() => {
+  function FilterBySalary() {
+    // fixedSalary
+    const FilteredValues = AllJobs?.filter(
+      (Job) =>
+        Job.fixedSalary >= FilteredSalaryValue.StartRange &&
+        Job.fixedSalary <= FilteredSalaryValue.EndRange
+    );
+
+    setFilteredBySalaryJobs(FilteredValues);
+  }
+  FilterBySalary();
+}, [FilteredSalaryValue]);
+
+//! FUNCTION FOR FETCHING THE LIST OF ALL THE COMPANIES THE USER HAS APPLIED TO
+useEffect(() => {
+  async function FetchCompaniesList() {
+    try {
+      const response = await fetch(
+        "https://nextjs-job-seeking-app.onrender.com/api/userRoutes/FetchCompaniesList",
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      const result = await response.json();
+
+      // if (result.JobList.length > 0) {
+      //   result.JobList.map((Job) => {
+      //     const JobDetails = await fetch(
+      //       "https://nextjs-job-seeking-app.onrender.com/api/jobRoutes/FindJobByID/:id"
+      //     );
+      //   });
+      // }
+
+      if (response.ok) {
+        setJobsAppliedTo(result.JobList);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  FetchCompaniesList();
+}, []);
+
+//! FUNCTION FOR POSTING AN APPLICATION
+async function SubmitJobApplication(
+  Name,
+  Email,
+  Resume,
+  Phone,
+  Address,
+  coverLetter,
+  jobId
+) {
+  try {
+    // Prepare FormData to handle file uploads
+    const formData = new FormData();
+    formData.append("Name", Name);
+    formData.append("Email", Email);
+    formData.append("Phone", Phone);
+    formData.append("Address", Address);
+    formData.append("coverLetter", coverLetter);
+    formData.append("Resume", Resume); // Add the file
+    formData.append("jobId", jobId);
+
+    const response = await fetch(
+      "https://nextjs-job-seeking-app.onrender.com/api/applicationRoutes/postApplication",
+      {
+        method: "POST",
+        credentials: "include",
+        body: formData,
+      }
+    );
+
+    const result = await response.json();
+    if (response.ok) {
+      setNewJobApplication(!NewJobApplication);
+      router.push("/");
+      toast.success("Successfully Applied!!");
+    } else {
+      toast.error(result.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+return (
+  <AppContext.Provider
+    value={{
+      JobsAppliedTo,
+      setUserDetails,
+      UserDetails,
+      setIsLoggedIn,
+      setNewJobPostedTracker,
+      HR_JobsListings,
+      AllJobs,
+      setDeleteJobTracker,
+      DeleteJobTracker,
+      setSearchJobText,
+      SearchJobValues,
+      setSearchJobValues,
+      SearchJobText,
+      SearchJobs,
+      setJobCategory,
+      FilteredJobCategory,
+      setIsFilteredByCategory,
+      IsFilteredByCategory,
+      setIsSearch,
+      IsSearch,
+      setIsLoading,
+      isLoading,
+      setFilteredSalaryValue,
+      FilteredSalaryValue,
+      FilteredBySalaryJobs,
+      setIsFilterBySalary,
+      IsFilterBySalary,
+      SubmitJobApplication,
+      setNewJobApplication,
+      NewJobApplication,
+      setListOfJobApplications,
+      ListOfJobApplications,
+    }}
+  >
+    {children}
+  </AppContext.Provider>
+);
 
 export default AppContextProvider;
